4102e64e3536ce516562a5f01f15bfed2379db62
Alispezzate <maurodarrigo882@gmail.com>
2021-07-19 02:21:35 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@
 import 'package:flutter/material.dart';
 import 'package:number_inc_dec/number_inc_dec.dart';
 import 'package:flutter/services.dart';
+import 'package:window_size/window_size.dart';
+
+
 
 
 void main() {
   runApp(const MyApp());
+  setWindowTitle("Garen GUI 3.0");
 }
 
 class MyApp extends StatelessWidget {
@@ -73,46 +77,49 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("Garen GUI 3.0"),
       ),
-      body: SizedBox(
-        width: 600,
-        child: Wrap(
-          runAlignment: WrapAlignment.center,
-          spacing: 20,
-          runSpacing: 20,
-          children: <Widget>[
-            NumberInputWithIncrementDecrement(
-              initialValue: 1,
-              controller: TextEditingController(),
-              onIncrement: (num lv) {
-                calculate(lv.toInt(), totHealth);
-                level=lv.toInt();
-              },
-              onDecrement: (num lv) {
-                calculate(lv.toInt(), totHealth);
-                level=lv.toInt();
-              },
-              min: 1,
-              max: 3,
-            ),
-            TextField(
-              controller: controllerMaxHP,
-              maxLength: 5,
-              onChanged: (value){
-                if(value.isNotEmpty){
-                  totHealth=int.parse(value);
-                  calculate(level, int.parse(value));
-                }
-              },
-              //initialValue: '0',
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter target maximum health points'
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: SizedBox(
+          width: 600,
+          child: Wrap(
+            runAlignment: WrapAlignment.center,
+            spacing: 20,
+            runSpacing: 20,
+            children: <Widget>[
+              NumberInputWithIncrementDecrement(
+                initialValue: 1,
+                controller: TextEditingController(),
+                onIncrement: (num lv) {
+                  calculate(lv.toInt(), totHealth);
+                  level=lv.toInt();
+                },
+                onDecrement: (num lv) {
+                  calculate(lv.toInt(), totHealth);
+                  level=lv.toInt();
+                },
+                min: 1,
+                max: 3,
               ),
-              keyboardType: TextInputType.number,
-            ),
-            Text("Optimal damage: $total"),
-          ],
+              TextField(
+                controller: controllerMaxHP,
+                maxLength: 5,
+                onChanged: (value){
+                  if(value.isNotEmpty){
+                    totHealth=int.parse(value);
+                    calculate(level, int.parse(value));
+                  }
+                },
+                //initialValue: '0',
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter target maximum health points'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              Text("Optimal damage: $total"),
+            ],
+          ),
         ),
       ),
     );
