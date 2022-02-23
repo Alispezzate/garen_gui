d3e56779ae67c9babe1fa30eb0f15ae0de3e7e83
Alispezzate <maurodarrigo882@gmail.com>
2022-02-23 19:09:23 +0100

    V0.3

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,13 @@ import 'package:flutter/services.dart';
 import 'package:window_size/window_size.dart';
 import 'dart:io' show Platform;
 
-
 void main() {
   runApp(const MyApp());
-  if(!kIsWeb){
-    if(Platform.isWindows){
+  if (!kIsWeb) {
+    if (Platform.isWindows) {
       setWindowTitle("Garen GUI 3.0");
     }
   }
-  
 }
 
 class MyApp extends StatelessWidget {
@@ -49,30 +47,30 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void calculate(int level, int totHealth) {
     setState(() {
-      switch (level){
+      switch (level) {
         case 1:
-            if(totHealth<=150){
-              total = totHealth;
-            }else{
+          if (totHealth <= 150) {
+            total = totHealth;
+          } else {
             total = ((totHealth + 600) ~/ 5);
-            }
-            break;
+          }
+          break;
         case 2:
-            if(totHealth<=300){
-              total = totHealth;
-            }else{
+          if (totHealth <= 300) {
+            total = totHealth;
+          } else {
             total = ((totHealth + 1000) * 3 ~/ 13);
-            }
-            break;
+          }
+          break;
         case 3:
-            if(totHealth<=450){
-              total = totHealth;
-            }else{
+          if (totHealth <= 450) {
+            total = totHealth;
+          } else {
             total = ((totHealth + 9000 / 7) * 7 ~/ 27);
-            }
-            break;
+          }
+          break;
         default:
-            break;
+          break;
       }
     });
   }
@@ -104,11 +102,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 initialValue: 1,
                 controller: TextEditingController(),
                 onIncrement: (num lv) {
-                  level=lv.toInt();
+                  level = lv.toInt();
                   calculate(level, totHealth);
                 },
                 onDecrement: (num lv) {
-                  level=lv.toInt();
+                  level = lv.toInt();
                   calculate(level, totHealth);
                 },
                 min: 1,
@@ -121,27 +119,36 @@ class _MyHomePageState extends State<MyHomePage> {
               Text(
                 "Target maximum health points",
                 style: TextStyle(height: 1, fontSize: 18),
-                ),
-              TextField(
-                controller: controllerMaxHP,
-                maxLength: 5,
-                onChanged: (value){
-                  if(value.isNotEmpty){
-                    totHealth=int.parse(value);
-                    calculate(level, totHealth);
+              ),
+              KeyboardListener(
+                focusNode: FocusNode(), // or FocusNode()
+                onKeyEvent: (event) {
+                  if (event.logicalKey == LogicalKeyboardKey.escape) {
+                    // here you can check if textfield is focused
+                    clearText();
                   }
                 },
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter target maximum health points',
-                  suffixIcon: IconButton(
-                    onPressed: controllerMaxHP.clear,
-                    icon: Icon(Icons.clear),
-                    splashRadius: 18,
+                child: TextField(
+                  controller: controllerMaxHP,
+                  maxLength: 5,
+                  onChanged: (value) {
+                    if (value.isNotEmpty) {
+                      totHealth = int.parse(value);
+                      calculate(level, totHealth);
+                    }
+                  },
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter target maximum health points',
+                    suffixIcon: IconButton(
+                      onPressed: controllerMaxHP.clear,
+                      icon: Icon(Icons.clear),
+                      splashRadius: 18,
+                    ),
                   ),
+                  keyboardType: TextInputType.number,
                 ),
-                keyboardType: TextInputType.number,
               ),
               Text(
                 "Optimal damage: $total",
