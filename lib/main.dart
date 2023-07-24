a7768306ed0291d33c5e05d59ae07e6295add1a8
Mauro D'Arrigo <mauro.darrigo@sys-datgroup.com>
2023-07-24 11:28:45 +0200

    added overlay switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,51 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart';
+import 'package:garen_gui/overlay.dart';
 import 'package:number_inc_dec/number_inc_dec.dart';
 import 'package:flutter/services.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:window_size/window_size.dart';
 import 'dart:io' show Platform;
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
   if (!kIsWeb) {
     if (Platform.isWindows) {
       setWindowTitle("Garen GUI 3.0");
     }
   }
+  if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+    doWhenWindowReady(() async {
+      const initialSize = Size(500, 500);
+      appWindow.minSize = initialSize;
+      appWindow.size = initialSize;
+      appWindow.title = "GAREN";
+      // appWindow.alignment = Alignment.centerRight;
+      appWindow.show();
+      // windowManager.setAlwaysOnTop(true);
+      // Window.setEffect(
+      //   effect: WindowEffect.transparent,
+      // );
+    });
+  }
+  // WindowOptions windowOptions = WindowOptions(
+  //   alwaysOnTop: true,
+  // );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // TODO: indagare sulla top bar di colore diverso
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: kDebugMode,
       title: 'Garen GUI 3.0',
       theme: ThemeData(
         brightness: Brightness.dark,
@@ -82,84 +105,120 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Garen GUI 3.0"),
-        backgroundColor: Color(0xFF212121),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(10.0),
-        alignment: Alignment.topCenter,
-        child: SizedBox(
-          width: 500,
-          child: Wrap(
-            runAlignment: WrapAlignment.center,
-            runSpacing: 10,
-            children: <Widget>[
-              Text(
-                "Garen ultimate level",
-                style: TextStyle(height: 1, fontSize: 18),
-              ),
-              NumberInputWithIncrementDecrement(
-                initialValue: 1,
-                controller: TextEditingController(),
-                onIncrement: (num lv) {
-                  level = lv.toInt();
-                  calculate(level, totHealth);
-                },
-                onDecrement: (num lv) {
-                  level = lv.toInt();
-                  calculate(level, totHealth);
-                },
-                min: 1,
-                max: 3,
-              ),
-              SizedBox(
-                height: 0,
-                width: 600,
-              ),
-              Text(
-                "Target maximum health points",
-                style: TextStyle(height: 1, fontSize: 18),
-              ),
-              KeyboardListener(
-                focusNode: FocusNode(), // or FocusNode()
-                onKeyEvent: (event) {
-                  if (event.logicalKey == LogicalKeyboardKey.escape) {
-                    // here you can check if textfield is focused
-                    clearText();
-                  }
-                },
-                child: TextField(
-                  controller: controllerMaxHP,
-                  maxLength: 5,
-                  onChanged: (value) {
-                    if (value.isNotEmpty) {
-                      totHealth = int.parse(value);
-                      calculate(level, totHealth);
+    return KeyboardListener(
+      onKeyEvent: (value) => value.logicalKey == LogicalKeyboardKey.delete ? showOverlay(context) : null,
+      focusNode: FocusNode(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Garen GUI 3.0"),
+          backgroundColor: Color(0xFF212121),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(10.0),
+          alignment: Alignment.topCenter,
+          child: SizedBox(
+            width: 500,
+            child: Wrap(
+              runAlignment: WrapAlignment.center,
+              runSpacing: 10,
+              children: <Widget>[
+                Text(
+                  "Garen ultimate level",
+                  style: TextStyle(height: 1, fontSize: 18),
+                ),
+                NumberInputWithIncrementDecrement(
+                  initialValue: 1,
+                  controller: TextEditingController(),
+                  onIncrement: (num lv) {
+                    level = lv.toInt();
+                    calculate(level, totHealth);
+                  },
+                  onDecrement: (num lv) {
+                    level = lv.toInt();
+                    calculate(level, totHealth);
+                  },
+                  min: 1,
+                  max: 3,
+                ),
+                SizedBox(
+                  height: 0,
+                  width: 600,
+                ),
+                Text(
+                  "Target maximum health points",
+                  style: TextStyle(height: 1, fontSize: 18),
+                ),
+                KeyboardListener(
+                  focusNode: FocusNode(), // or FocusNode()
+                  onKeyEvent: (event) {
+                    if (event.logicalKey == LogicalKeyboardKey.escape) {
+                      // here you can check if textfield is focused
+                      clearText();
                     }
                   },
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter target maximum health points',
-                    suffixIcon: IconButton(
-                      onPressed: controllerMaxHP.clear,
-                      icon: Icon(Icons.clear),
-                      splashRadius: 18,
+                  child: TextField(
+                    controller: controllerMaxHP,
+                    maxLength: 5,
+                    onChanged: (value) {
+                      if (value.isNotEmpty) {
+                        totHealth = int.parse(value);
+                        calculate(level, totHealth);
+                      }
+                    },
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter target maximum health points',
+                      suffixIcon: IconButton(
+                        onPressed: controllerMaxHP.clear,
+                        icon: Icon(Icons.clear),
+                        splashRadius: 18,
+                      ),
                     ),
+                    keyboardType: TextInputType.number,
                   ),
-                  keyboardType: TextInputType.number,
                 ),
-              ),
-              Text(
-                "Optimal damage: $total",
-                style: TextStyle(height: 1, fontSize: 18),
-              ),
-            ],
+                Column(
+                  children: [
+                    Text(
+                      "Optimal damage: $total",
+                      style: TextStyle(height: 1, fontSize: 18),
+                    ),
+                    Text(
+                      "Press delete to show overlay",
+                      style: TextStyle(height: 1, fontSize: 18),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  showOverlay(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => WindowOverlay(),
+      ),
+    );
+    if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+      doWhenWindowReady(() async {
+        appWindow.hide();
+        const initialSize = Size(170, 220);
+        appWindow.minSize = initialSize;
+        appWindow.size = initialSize;
+        appWindow.title = "GAREN";
+        appWindow.alignment = Alignment.centerRight;
+        // appWindow.restore();
+        appWindow.show();
+        // windowManager.setAlwaysOnTop(true);
+        Window.setEffect(
+          effect: WindowEffect.transparent,
+        );
+      });
+    }
   }
 }
---
+++ lib/overlay.dart
@@ -0,0 +1,286 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
+import 'package:flutter/services.dart';
+import 'dart:io' show File, Platform, Process;
+import 'package:window_manager/window_manager.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:image/image.dart' as img;
+
+class Enemy {
+  int hp = 0;
+  int maxHp = 0;
+}
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await windowManager.ensureInitialized();
+//   await Window.initialize();
+//   runApp(const MyApp());
+//   // MyApp.
+//   if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+//     doWhenWindowReady(() async {
+//       const initialSize = Size(170, 220);
+//       appWindow.minSize = initialSize;
+//       appWindow.size = initialSize;
+//       appWindow.title = "GAREN";
+//       appWindow.alignment = Alignment.centerRight;
+//       appWindow.show();
+//       windowManager.setAlwaysOnTop(true);
+//       Window.setEffect(
+//         effect: WindowEffect.transparent,
+//       );
+//     });
+//   }
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Garen GUI 3.0',
+//       theme: ThemeData(
+//         brightness: Brightness.dark,
+//         primarySwatch: Colors.green,
+//       ),
+//       home: const WindowOverlay(),
+//     );
+//   }
+// }
+
+class WindowOverlay extends StatefulWidget {
+  const WindowOverlay({Key? key}) : super(key: key);
+
+  @override
+  State<WindowOverlay> createState() => _WindowOverlayState();
+}
+
+class _WindowOverlayState extends State<WindowOverlay> {
+  int totHealth = 0;
+  int total = 0;
+  int level = 1;
+  final controllerMaxHP = TextEditingController();
+  final controllerLevel = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // TODO: aggiungere la possibilità di personalizzare la dimensione e posizione dello screenshot
+  // TODO: migliorare ocr, testare https://pub.dev/packages?q=platform%3Awindows+ocr
+  // TODO: automatizzare inserimento livello ultimate
+  // TODO: cancellare file inutili
+  // TODO: usare https://127.0.0.1:2999/liveclientdata/allgamedata per ottenere il livello della ulti
+  // TODO: mostrare hp attuali
+  Future<void> ocr(TextEditingController level, Enemy enemy, TextEditingController controllerMaxHp, KeyEvent event) async {
+    String nirPath;
+    String ocrPath;
+    if (kDebugMode) {
+      nirPath = "lib/nircmd-x64/nircmd.exe";
+      ocrPath = "lib/Tesseract-OCR/tesseract.exe";
+    } else {
+      nirPath = "nircmd-x64/nircmd.exe";
+      ocrPath = "Tesseract-OCR/tesseract.exe";
+      controllerMaxHP.text = "69420";
+    }
+    while (true) {
+      // wait 1 second
+      // await Future.delayed(const Duration(milliseconds: 100));
+      // TODO: usare https://pub.dev/packages/win32 screenshot.dart , o meglio, https://pub.dev/packages/screen_capturer
+      dynamic process = await Process.start(nirPath, ['savescreenshot', 'screen1.png', '230', '40', '150', '25']);
+      // stdout.addStream(process.stdout);
+      // stderr.addStream(process.stderr);
+      await process.exitCode;
+      try {
+        // turn image to gray scale
+        img.Image? image = img.decodePng(File('screen1.png').readAsBytesSync());
+        image = img.invert(image!);
+        image = img.adjustColor(image, gamma: 0.179, saturation: 0.0);
+        // image = img.grayscale(image);
+        // save image
+        File('screen1.png').writeAsBytesSync(img.encodePng(image));
+        process = await Process.start(ocrPath, ['screen1.png', 'text', '-l', 'eng', 'digits']);
+      } catch (e) {
+        print(e);
+      }
+      // stdout.addStream(process.stdout);
+      // stderr.addStream(process.stderr);
+      await process.exitCode;
+      // extract string from file with regex and print it
+      RegExp regExp = RegExp(r'\d+\/\d+');
+      String text = File('text.txt').readAsStringSync();
+      if (kDebugMode) {
+        print(regExp.stringMatch(text));
+      }
+      try {
+        enemy.hp = int.tryParse((regExp.stringMatch(text)!.replaceAll(RegExp(r'\/\d+'), ''))) ?? 0;
+        print("enemy hp: " + enemy.hp.toString());
+        enemy.maxHp = int.tryParse((regExp.stringMatch(text)!.replaceAll(RegExp(r'\d+\/'), ''))) ?? 0;
+        print("enemy max hp: " + enemy.maxHp.toString());
+        controllerMaxHP.text = enemy.maxHp.toString();
+        calculate(int.parse(level.text), enemy.maxHp);
+      } catch (e) {
+        enemy.hp = 0;
+        enemy.maxHp = 0;
+      }
+    }
+  }
+
+  void calculate(int level, int totHealth) {
+    setState(() {
+      switch (level) {
+        case 1:
+          if (totHealth <= 150) {
+            total = totHealth;
+          } else {
+            total = ((totHealth + 600) ~/ 5);
+          }
+          break;
+        case 2:
+          if (totHealth <= 300) {
+            total = totHealth;
+          } else {
+            total = ((totHealth + 1000) * 3 ~/ 13);
+          }
+          break;
+        case 3:
+          if (totHealth <= 450) {
+            total = totHealth;
+          } else {
+            total = ((totHealth + 9000 / 7) * 7 ~/ 27);
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  void clearText() {
+    controllerMaxHP.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyboardListener(
+      onKeyEvent: (value) => value.logicalKey == LogicalKeyboardKey.pageUp ? getBackToMainWindow(context) : null,
+      focusNode: FocusNode(),
+      child: SizedBox(
+          width: 200,
+          child: Column(
+            children: [
+              WindowTitleBarBox(
+                  child: Stack(
+                children: [
+                  Container(
+                    color: const Color.fromARGB(140, 0, 0, 0),
+                  ),
+                  MoveWindow(),
+                ],
+              )),
+              Expanded(
+                  child: Scaffold(
+                backgroundColor: const Color.fromARGB(110, 0, 0, 0),
+                body: Container(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 1.0),
+                  alignment: Alignment.topCenter,
+                  child: SizedBox(
+                    width: 500,
+                    child: Wrap(
+                      runAlignment: WrapAlignment.center,
+                      runSpacing: 5,
+                      children: <Widget>[
+                        const Text(
+                          "Garen ultimate level",
+                          style: TextStyle(height: 1, fontSize: 11),
+                        ),
+                        NumberInputWithIncrementDecrement(
+                          initialValue: 1,
+                          controller: controllerLevel,
+                          onIncrement: (num lv) {
+                            level = lv.toInt();
+                            calculate(level, totHealth);
+                          },
+                          onDecrement: (num lv) {
+                            level = lv.toInt();
+                            calculate(level, totHealth);
+                          },
+                          min: 1,
+                          max: 3,
+                        ),
+                        const Text(
+                          "Target maximum health points",
+                          style: TextStyle(height: 1, fontSize: 11),
+                        ),
+                        KeyboardListener(
+                          focusNode: FocusNode(), // or FocusNode()
+                          onKeyEvent: (event) {
+                            if (event.logicalKey == LogicalKeyboardKey.escape) {
+                              // here you can check if textfield is focused
+                              // clearText();
+                              Enemy enemy = Enemy();
+
+                              ocr(controllerLevel, enemy, controllerMaxHP, event);
+                            }
+                          },
+                          child: TextField(
+                            controller: controllerMaxHP,
+                            maxLength: 5,
+                            onChanged: (value) {
+                              if (value.isNotEmpty) {
+                                totHealth = int.parse(value);
+                                calculate(level, totHealth);
+                              }
+                            },
+                            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.only(left: 5),
+                              border: const OutlineInputBorder(),
+                              hintText: 'Enter target maximum health points',
+                              suffixIcon: IconButton(
+                                onPressed: controllerMaxHP.clear,
+                                icon: const Icon(Icons.clear),
+                                splashRadius: 18,
+                              ),
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "Optimal damage: $total",
+                              style: const TextStyle(height: 1, fontSize: 11),
+                            ),
+                            Text(
+                              "Press Page Up to hide overlay",
+                              style: TextStyle(height: 1, fontSize: 11),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ))
+            ],
+          )),
+    );
+  }
+
+  getBackToMainWindow(BuildContext context) {
+    appWindow.hide();
+    Navigator.of(context).pop();
+    const initialSize = Size(500, 500);
+    appWindow.minSize = initialSize;
+    appWindow.size = initialSize;
+    appWindow.title = "GAREN";
+    appWindow.alignment = Alignment.center;
+    appWindow.show();
+    // windowManager.setAlwaysOnTop(true);
+  }
+}
