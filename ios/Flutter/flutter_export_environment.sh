d1d659985fa4db168f4857b9c64990cf103df272
Alispezzate <38697944+Alispezzate@users.noreply.github.com>
2023-07-25 08:32:28 +0200

    icons speculative fix

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Mauro\fvm\versions\3.10.6"
+export "FLUTTER_ROOT=C:\Users\Mauro\fvm\versions\3.10.0"
 export "FLUTTER_APPLICATION_PATH=E:\disco d\garen_gui"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.5.0"
-export "FLUTTER_BUILD_NUMBER=0"
+export "FLUTTER_BUILD_NAME=0.5.1"
+export "FLUTTER_BUILD_NUMBER=2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
